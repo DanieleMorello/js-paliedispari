e1d927b479369e019cb0a9de3c25bffc36df27c8
DanieleMorello <danielemorello@live.it>
2023-03-16 18:34:45 +0100

    fix function isPalindrome

--- palidromo/js/main.js
+++ palidromo/js/main.js
@@ -9,31 +9,32 @@ const userWord = prompt("Inserisci una parola");
 // Creare una funzione per capire se la parola inserita è palindroma.
 
 // Creo la funzione isPalindrome
-function isPalindrome(word) {
+function isPalindrome(Word) {
   // creo un array e lo riempio con la parola scritta dall' utente
-  const wordLetters = [];
-  wordLetters.push(word);
-  console.log(wordLetters);
+  const wordLetters = []
+  wordLetters.push(Word)
+ 
 
   // Creo una variabile carattere e inserisco ddento di essa le singole letter della parola scritta dall'utente
-  let wordLetter = word.split("");
-  console.log(wordLetter);
+  let wordLetter = userWord.split("")
+ 
 
   // Creo una variabile e inserisco le lettere nell' ordine inverso
-  let inverseWordLetter = wordLetter.reverse();
-  console.log(inverseWordLetter);
+  let inverseWordLetter = wordLetter.reverse()
+
 
   // Riunisco le lettere della parola inversa e li salvo in una variabile
-  const inverseWordUser = inverseWordLetter.join("");
-  console.log(inverseWordUser);
+  const inverseWord = inverseWordLetter.join("")
+ 
 
   // Stabilisco l condizioni per cui la parola è palindroma
-  if (word === inverseWordUser) {
-    console.log(`${word} è palindroma`);
+  if (Word === inverseWord) {
+    return true
   } else {
-    console.log(`${word} non è palindroma`);
+    return false
   }
 }
 
 // Invoco la funzione
-isPalindrome(userWord);
+console.log(isPalindrome(userWord));
+console.log(isPalindrome('asso'));
