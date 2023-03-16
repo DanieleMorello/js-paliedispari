d4e0f2416f504223c44a9556690e49e62b3469fd
DanieleMorello <danielemorello@live.it>
2023-03-16 16:07:26 +0100

    Insert a prompt asking you to enter a word

--- palidromo/js/main.js
+++ palidromo/js/main.js
@@ -2,3 +2,6 @@
   Chiedere all’utente di inserire una parola.
   Creare una funzione per capire se la parola inserita è palindroma
  */
+
+  // Chiedere all’utente di inserire una parola.
+  const userWord = prompt('Inserisci una parola')
