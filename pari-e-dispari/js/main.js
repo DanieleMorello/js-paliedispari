8e9d50f4853e309c319e421e9e9f6c7e55821d73
DanieleMorello <danielemorello@live.it>
2023-03-16 15:31:35 +0100

    Create sum function

--- pari-e-dispari/js/main.js
+++ pari-e-dispari/js/main.js
@@ -7,16 +7,32 @@
 
 // L’utente sceglie pari o dispari. 
 const userChoice = prompt('Scegli pari(p) o dispari (d)')
-console.log(userChoice)
+console.log('Scelta utente: ',userChoice)
 
 // L’utente inserisce un numero da 1 a 5.
 const userNumb = Number(prompt('Inserisci un numero da 1 a 5'))
-console.log(userNumb)
+console.log('Numero utente: ',userNumb)
 
 // Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
 function randomNumber(min,max){ //Funzione googlata
-  return Math.floor(Math.random()*(max-min+1)+min)
+  const randomNum = Math.floor(Math.random()*(max-min+1)+min)
+  return randomNum
 }
 
 // Assegno il valore della funzione ad una costante
-const pcNumb = console.log(randomNumber(1, 5))
+const pcNumb = randomNumber(1, 5)
+console.log('Numero random pc: ',pcNumb);
+
+
+//Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+function sum(numb1, numb2) {
+  const result = (numb1 + numb2)
+  if (result % 2 == 0) {
+    console.log(`La somma tra ${userNumb} e ${pcNumb} è pari`);
+  } else {
+    console.log(`La somma tra ${userNumb} e ${pcNumb} è dispari`);
+  }
+  return result
+}
+
+console.log(sum(userNumb, pcNumb));
