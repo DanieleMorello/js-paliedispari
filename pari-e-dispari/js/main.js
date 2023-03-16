e26fdf651166cafc2b907adbb331aaf3972b1e55
DanieleMorello <danielemorello@live.it>
2023-03-16 14:53:36 +0100

    create function randomNumber, create the variable pcNumb assign it the value of the function

--- pari-e-dispari/js/main.js
+++ pari-e-dispari/js/main.js
@@ -7,8 +7,16 @@
 
 // L’utente sceglie pari o dispari. 
 const userChoice = prompt('Scegli pari(p) o dispari (d)')
-console.log(userChoice);
+console.log(userChoice)
 
 // L’utente inserisce un numero da 1 a 5.
 const userNumb = Number(prompt('Inserisci un numero da 1 a 5'))
 console.log(userNumb)
+
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+function randomNumber(min,max){ //Funzione googlata
+  return Math.floor(Math.random()*(max-min+1)+min)
+}
+
+// Assegno il valore della funzione ad una costante
+const pcNumb = console.log(randomNumber(1, 5))
