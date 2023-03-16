85315b68dda15177e0ebcc2446e1f3b9bf1b87d7
DanieleMorello <danielemorello@live.it>
2023-03-16 19:01:23 +0100

    Fix function isPalindrome and fix pari e dispari js folder

--- palidromo/js/main.js
+++ palidromo/js/main.js
@@ -11,30 +11,26 @@ const userWord = prompt("Inserisci una parola");
 // Creo la funzione isPalindrome
 function isPalindrome(Word) {
   // creo un array e lo riempio con la parola scritta dall' utente
-  const wordLetters = []
-  wordLetters.push(Word)
- 
+  const wordLetters = [];
+  wordLetters.push(Word);
 
   // Creo una variabile carattere e inserisco ddento di essa le singole letter della parola scritta dall'utente
-  let wordLetter = userWord.split("")
- 
+  let wordLetter = userWord.split("");
 
   // Creo una variabile e inserisco le lettere nell' ordine inverso
-  let inverseWordLetter = wordLetter.reverse()
-
+  let inverseWordLetter = wordLetter.reverse();
 
   // Riunisco le lettere della parola inversa e li salvo in una variabile
-  const inverseWord = inverseWordLetter.join("")
- 
+  const inverseWord = inverseWordLetter.join("");
 
   // Stabilisco l condizioni per cui la parola è palindroma
   if (Word === inverseWord) {
-    return true
+    return true;
   } else {
-    return false
+    return false;
   }
 }
 
 // Invoco la funzione
 console.log(isPalindrome(userWord));
-console.log(isPalindrome('asso'));
+console.log(isPalindrome("asso"));
--- pari-e-dispari/js/main.js
+++ pari-e-dispari/js/main.js
@@ -5,36 +5,38 @@
   Dichiariamo chi ha vinto.
  */
 
-// L’utente sceglie pari o dispari. 
-const userChoice = prompt('Scegli pari o dispari')
-console.log('Scelta utente: ',userChoice)
+// L’utente sceglie pari o dispari.
+const userChoice = prompt("Scegli pari o dispari");
+console.log("Scelta utente: ", userChoice);
 
 // L’utente inserisce un numero da 1 a 5.
-const userNumb = Number(prompt('Inserisci un numero da 1 a 5'))
-console.log('Numero utente: ',userNumb)
+const userNumb = Number(prompt("Inserisci un numero da 1 a 5"));
+console.log("Numero utente: ", userNumb);
 
 // Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
-function randomNumber(min,max){ //Funzione googlata
-  const randomNum = Math.floor(Math.random()*(max-min+1)+min)
-  return randomNum
+function randomNumber(min, max) {
+  //Funzione googlata
+  const randomNum = Math.floor(Math.random() * (max - min + 1) + min);
+  return randomNum;
 }
 
 // Assegno il valore della funzione ad una costante
-const pcNumb = randomNumber(1, 5)
-console.log('Numero random pc: ',pcNumb);
-let flag = 'dispari'
+const pcNumb = randomNumber(1, 5);
+console.log("Numero random pc: ", pcNumb);
+
 //Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 function sum(numb1, numb2) {
-  const result = (numb1 + numb2)
-  console.log('Somma: ',result,`${flag}`);
-  if (result % 2 == 0) {
-    flag = 'pari'
-  }
-  userChoice === flag ? console.log('Congratulation: You Win!!!') : console.log('Sorry Pc Wins!!!')
-  
+  const result = numb1 + numb2;
+  return result;
 }
 
 // Salvo il risultato in una variabile
-const somma = sum(userNumb, pcNumb)
+const somma = sum(userNumb, pcNumb);
 
-
+let flag = "dispari";
+if (somma % 2 == 0) {
+  flag = "pari";
+}
+userChoice === flag
+  ? console.log("Congratulation: You Win!!!")
+  : console.log("Sorry Pc Wins!!!");
