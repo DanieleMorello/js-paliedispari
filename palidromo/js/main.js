4999fa0eae87482130615163922fa3f47db65757
DanieleMorello <danielemorello@live.it>
2023-03-16 17:23:59 +0100

    Transferred all the instructions inside the function and called it

--- palidromo/js/main.js
+++ palidromo/js/main.js
@@ -7,30 +7,36 @@
   const userWord = prompt('Inserisci una parola')
 
   // Creare una funzione per capire se la parola inserita è palindroma.
-  // creo un array e lo riempio con la parola scritta dall' utente
-  const wordLetters = []
-  wordLetters.push(userWord)
-  console.log(wordLetters)
+ 
 
-  // Creo una variabile carattere e inserisco ddento di essa le singole letter della parola scritta dall'utente
-  let wordLetter = userWord.split('')
-  console.log(wordLetter);
+  // Creo la funzione isPalindrome
+  function isPalindrome() {
+     // creo un array e lo riempio con la parola scritta dall' utente
+    const wordLetters = []
+    wordLetters.push(userWord)
+    console.log(wordLetters)
 
-  // Creo una variabile e inserisco le lettere nell' ordine inverso
-  let inverseWordLetter = wordLetter.reverse()
-  console.log(inverseWordLetter);
+    // Creo una variabile carattere e inserisco ddento di essa le singole letter della parola scritta dall'utente
+    let wordLetter = userWord.split('')
+    console.log(wordLetter);
 
-  // Riunisco le lettere della parola inversa e li salvo in una variabile
-  const inverseWordUser = inverseWordLetter.join('')
-  console.log(inverseWordUser)
+    // Creo una variabile e inserisco le lettere nell' ordine inverso
+    let inverseWordLetter = wordLetter.reverse()
+   console.log(inverseWordLetter);
 
-  // Stabilisco l condizioni per cui la parola è palindroma
-  if (userWord === inverseWordUser) {
+   // Riunisco le lettere della parola inversa e li salvo in una variabile
+    const inverseWordUser = inverseWordLetter.join('')
+    console.log(inverseWordUser)
+
+    // Stabilisco l condizioni per cui la parola è palindroma
+    if (userWord === inverseWordUser) {
     console.log(`${userWord} è palindroma`)
-  } else {
+    } else {
     console.log(`${userWord} non è palindroma`)
+    }
   }
 
+  isPalindrome(userWord)
  
 
 
